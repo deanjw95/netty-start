fbceee2902b7f8007ae929f69955e75cc0a9297a
Jeong Wook Byun <deanjw95@naver.com>
2023-05-15 17:07:35 +0900

    first commit

---
+++ src/test/java/com/java/nettystart/NettyStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.nettystart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
